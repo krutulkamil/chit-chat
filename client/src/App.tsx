e20a404fa47cb1c6be7ab084865cb5ee64914ad4
krutulkamil <79461468+krutulkamil@users.noreply.github.com>
2022-03-26 15:39:42 +0100

    ChakraUI dark mode toggle button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import React, {FC} from 'react';
+import ToggleColorMode from "./components/ToggleColorMode";
 
 const App: FC = (): JSX.Element => {
-  return (
-      <div>TEST</div>
-  )
+    return (
+        <>
+            <ToggleColorMode />
+        </>
+    )
 }
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { ColorModeScript } from "@chakra-ui/color-mode";
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import theme from "./theme";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ChakraProvider theme={theme}>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            <App />
+        </ChakraProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
